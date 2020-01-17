62a50326b4b751b0d90d8de7ca0fb01c74fb7144
Rafael Schouten <rafaelschouten@gmail.com>
2020-01-17 17:39:12 +1100

    add sum output

---
+++ src/outputs.jl
@@ -0,0 +1,17 @@
+"""
+An output that calculates the total cost at each step
+
+The grid is not saved, just a single float value.
+"""
+DynamicGrids.@Output mutable struct SumOutput{} <: Output{T}
+    gridname::Symbol | :cost
+end
+SumOutput(length::Integer; kwargs...) = SumOutput(; frames=zeros(length), kwargs...)
+
+DynamicGrids.storeframe!(output::SumOutput, data::MultiSimData, f) = begin
+    checkbounds(output, f)
+    output[f] = sum(data[output.gridname])
+end
+
+DynamicGrids.initframes!(output::SumOutput, init::NamedTuple) =
+    output[1] = sum(init[output.gridname])
